b4069015b4c423f01bf56e2e8aca09a82d0683c1
tinkai <jyamusonkouki@gmail.com>
2019-02-28 17:33:20 +0900

    機体の旋回を実装

---
+++ Missile Fighter/Assets/Scripts/Fighter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    // 機体を制御するリキッドボディ
+    private Rigidbody fighterbody;
+
+    // 上昇・下降・旋回に対する力
+    [SerializeField] private float risingForce = 3.0f;
+    [SerializeField] private float fallingForce = 1.0f;
+    [SerializeField] private float rollingForce = 5.0f;
+
+
+    void Start()
+    {
+        fighterbody = GetComponent<Rigidbody>();
+    }
+
+    // 機体の上昇メソッド
+    public void Rise(float vertical)
+    {
+        fighterbody.AddTorque(transform.right * vertical * risingForce);
+    }
+
+    // 機体の下降メソッド
+    public void Fall(float vertical)
+    {
+        fighterbody.AddTorque(transform.right * vertical * fallingForce);
+    }
+
+    // 機体を旋回させるメソッド
+    public void Roll(float horizontal)
+    {
+        fighterbody.AddTorque(transform.forward * horizontal * rollingForce);
+    }
+}
---
+++ Missile Fighter/Assets/Scripts/FighterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterController : MonoBehaviour
+{
+    // 操作する戦闘機
+    [SerializeField] private Fighter fighter;
+
+
+    // 描画前に呼ばれるUpdate
+    void FixedUpdate()
+    {
+        InputRoll();
+        InputVertical();
+    }
+
+    // 旋回に関する入力処理
+    void InputRoll()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        fighter.Roll(-horizontal);
+    }
+
+    // 上昇下降に関する入力処理
+    void InputVertical()
+    {
+        float vertical = Input.GetAxisRaw("Vertical");
+        if (vertical >= 0)
+        {
+            fighter.Fall(vertical);
+        }
+        else
+        {
+            fighter.Rise(vertical);
+        }
+    }
+}
