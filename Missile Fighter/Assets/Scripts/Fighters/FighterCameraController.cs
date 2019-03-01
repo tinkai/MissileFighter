373677460a64408541642da975c9c64fd5112bbc
tinkai <jyamusonkouki@gmail.com>
2019-03-01 21:28:20 +0900

    カメラが３人称視点と１人称視点を切り替えることができるように変更

---
+++ Missile Fighter/Assets/Scripts/Fighters/FighterCameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fighters
+{
+    public class FighterCameraController : MonoBehaviour
+    {
+        // カメラオブジェクト
+        [SerializeField] private GameObject thirdPersonCamera;  // 通常視点
+        [SerializeField] private GameObject firstPersonCamera;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.LeftAlt))
+            {
+                // 現在のactive状態から反転 
+                thirdPersonCamera.SetActive(!thirdPersonCamera.activeInHierarchy);
+                firstPersonCamera.SetActive(!firstPersonCamera.activeInHierarchy);
+            }
+        }
+    }
+}
