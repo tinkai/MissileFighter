559d4b5920e2b03ddd2f05cb0710df076f494cbc
tinkai <jyamusonkouki@gmail.com>
2019-03-01 16:51:45 +0900

    誘導ミサイルに変更

--- Missile Fighter/Assets/Scripts/Missile.cs
+++ Missile Fighter/Assets/Scripts/Missile.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
+    // ミサイルのリキッドボディ
     private Rigidbody missilebody;
+
+    // ミサイルの敵ターゲット
+    private Transform target;
+
+    // ミサイルの敵への誘導率
+    [SerializeField] private float inductionRate = 1.0f;
+
+    // ミサイルの射出時の下への動き
+    [SerializeField] private float shotForce = 10.0f;
 
 
     void Start()
     {
         missilebody = gameObject.GetComponent<Rigidbody>();
+        target = GameObject.Find("Anemy").transform;
 
-        // 機体の速力以上にする
+        // 機体の速力にする
         GameObject fighter = GameObject.Find("Player Fighter");
-        missilebody.velocity = fighter.GetComponent<Rigidbody>().velocity + fighter.transform.forward * 100;
+        // 下に射出
+        missilebody.velocity = fighter.GetComponent<Rigidbody>().velocity + fighter.transform.up * (-shotForce);
     }
 
     void Update()
     {
-        
+        GuidedTarget();
+    }
+
+    void GuidedTarget()
+    {
+        Quaternion targetDirection = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetDirection, inductionRate);
+        missilebody.AddForce(transform.forward * 100);
     }
 }
