1f913dd1ddc37fae69c07be626531de83743439e
tinkai <jyamusonkouki@gmail.com>
2019-03-01 22:49:29 +0900

    ミサイルの発射時の停止挙動を追加

--- Missile Fighter/Assets/Scripts/Missiles/Missile.cs
+++ Missile Fighter/Assets/Scripts/Missiles/Missile.cs
@@ -12,12 +12,25 @@ namespace Missiles
         // ミサイルの敵ターゲット
         private Transform target;
 
+        // ミサイルの速度
+        [SerializeField] private float speed = 400.0f;
+
         // ミサイルの敵への誘導率
-        [SerializeField] private float inductionRate = 1.0f;
+        [SerializeField] private float inductionRate = 0.1f;
+
+        // ミサイルが誘導を始めるまでの移動方向と速度
+        private Vector3 shotStartVector;
 
         // ミサイルの射出時の下への動き
-        [SerializeField] private float shotForce = 10.0f;
+        [SerializeField] private float shotForce = 3.0f;
 
+        // ミサイルが誘導を始めるまでの間隔
+        [SerializeField] private float guidDelayTime = 1.0f;
+
+        // ミサイルが誘導を始める時間
+        private float startGuidTime;
+
+        
 
         void Start()
         {
@@ -25,21 +38,35 @@ namespace Missiles
             target = GameObject.Find("Anemy").transform;
 
             // 機体の速力にする
-            GameObject fighter = GameObject.Find("Player Fighter");
-            // 下に射出
-            missilebody.velocity = fighter.GetComponent<Rigidbody>().velocity + fighter.transform.up * (-shotForce);
+            shotStartVector = GameObject.Find("Player Fighter").GetComponent<Rigidbody>().velocity;
+            // 機体速度と同速で下に射出
+            missilebody.velocity = shotStartVector - transform.up * shotForce;
+
+            // 誘導を始める時間を設定
+            startGuidTime = Time.time + guidDelayTime;
         }
 
-        void Update()
+        void FixedUpdate()
         {
-            GuidedTarget();
+            if (Time.time >= startGuidTime)
+            {
+                GuidedTarget();
+            }
+            else
+            {
+                missilebody.AddForce(shotStartVector);
+            }
         }
 
+        // ミサイルをターゲットに誘導するメソッド
         void GuidedTarget()
         {
+            // 自分自身からターゲットを見た方向を取得
             Quaternion targetDirection = Quaternion.LookRotation(target.position - transform.position);
+            // 自分の向いている方向からターゲット方向へ誘導率だけ向く
             transform.rotation = Quaternion.Slerp(transform.rotation, targetDirection, inductionRate);
-            missilebody.AddForce(transform.forward * 100);
+            // 正面に力を加える
+            missilebody.AddForce(transform.forward * speed);
         }
     }
 }
